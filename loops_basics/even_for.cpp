5754e3d6b72bd5a58b2fc7c1b42fd75e8e11de04
patilrohit1964 <patilrohit19649@gmail.com>
2025-02-05 23:19:05 +0530

    added even file

---
+++ loops_basics/even_for.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    for (int i = 1; i <= 10; i++)
+    {
+        if (i % 2 == 0)
+        {
+            cout << i << " is even" << endl;
+        }
+        else
+        {
+            cout << i << " is odd" << endl;
+        }
+    }
+}
