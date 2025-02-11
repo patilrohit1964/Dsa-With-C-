44acc93d64d017b571f8b523e916accd6dd24a44
patilrohit1964 <patilrohit19649@gmail.com>
2025-02-11 23:46:44 +0530

    added loops

---
+++ pattern_basics_ch7/pattern16.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << ((i + j) % 2) << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
