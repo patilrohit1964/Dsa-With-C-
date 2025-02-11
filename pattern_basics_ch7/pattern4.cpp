3e90a6d11b0860315db4cf027008ed55ac6464dd
patilrohit1964 <patilrohit19649@gmail.com>
2025-02-11 23:34:26 +0530

    added loops

---
+++ pattern_basics_ch7/pattern4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = n; j >= n - i + 1; j--)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
