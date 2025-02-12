7b1a3dfbf84019b657676c7e34cc48c0078b1728
patilrohit1964 <patilrohit19649@gmail.com>
2025-02-12 23:32:17 +0530

    added space loops

---
+++ pattern_space_ch8/pattern3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = 1; j < i; j++)
+            cout << "  ";
+        for (int j = i; j <= n; j++)
+            cout << j << " ";
+        cout << endl;
+    }
+    return 0;
+}
