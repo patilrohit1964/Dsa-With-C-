df9e1af9033e25a370f4150d6876e848ad920bab
patilrohit1964 <patilrohit19649@gmail.com>
2025-02-14 00:01:04 +0530

    added patterns

---
+++ pattern_space_ch8/pattern23.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = i; j <= 5; j++)
+        {
+            cout << "  ";
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            if (j % 2 == 0)
+            {
+                cout << "-" << " ";
+            }
+            else
+            {
+                cout << "|" << " ";
+            }
+        }
+        cout << endl;
+    }
+}
