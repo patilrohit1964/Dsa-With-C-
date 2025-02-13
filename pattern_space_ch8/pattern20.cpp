4e12a85d816d7cce8237dc745a411a6f50cd6d0a
patilrohit1964 <patilrohit19649@gmail.com>
2025-02-13 23:57:23 +0530

    added patterns

---
+++ pattern_space_ch8/pattern20.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int rows = 5;
+    for (int i = rows; i >= 1; i--)
+    {
+        for (int j = 0; j < rows - i; j++)
+        {
+            cout << "  ";
+        }
+        for (int j = 0; j < i; j++)
+        {
+            cout << ((j % 2) == 0 ? 1 : 0) << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
