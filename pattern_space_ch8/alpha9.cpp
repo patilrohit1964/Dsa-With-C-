9594f4182639d6193a401b6f4da322670574f539
patilrohit1964 <patilrohit19649@gmail.com>
2025-02-14 23:44:33 +0530

    added patterns

---
+++ pattern_space_ch8/alpha9.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i, j;
+    for (i = 0; i < 7; i++)
+    {
+        for (j = 0; j < 5; j++)
+        {
+            if (i == 0 || i == 6 || j == 2)
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
