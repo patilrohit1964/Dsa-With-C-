26a539851d7cf6c3bf52f9dc43158e86474b74af
patilrohit1964 <patilrohit19649@gmail.com>
2025-02-14 23:52:03 +0530

    added patterns

---
+++ pattern_space_ch8/alpha19.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i, j;
+    for (i = 0; i < 7; i++)
+    {
+        for (j = 0; j < 5; j++)
+        {
+            if ((i == 0 || i == 3 || i == 6) || (j == 0 && i < 3) || (j == 4 && i > 3))
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
