62e5dee8190167bddb1ce8862b3ef0a3b6cc7c52
patilrohit1964 <patilrohit19649@gmail.com>
2025-02-19 23:56:28 +0530

    min max added

--- array1dPrac_ch10/arr_min_max.cpp
+++ array1dPrac_ch10/arr_min_max.cpp
@@ -4,19 +4,30 @@ using namespace std;
 int main()
 {
     int n;
+
     cout << "Enter the size of the array: ";
     cin >> n;
+
     int arr[n];
+
     cout << "Enter " << n << " elements: ";
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
-        cout << "Reverse Number of array: ";
-    for (int i = n-1; i >=0; i--)
+
+    int min = arr[0], max = arr[0];
+
+    for (int i = 1; i < n; i++)
     {
-        cout << arr[i]<< " "; 
+        if (arr[i] < min)
+            min = arr[i];
+        if (arr[i] > max)
+            max = arr[i];
     }
+
+    cout << "Minimum element: " << min << endl;
+    cout << "Maximum element: " << max << endl;
 
     return 0;
 }
