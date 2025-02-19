fee7dabf1893a77e78e4f2f9716a5c7e594d256b
patilrohit1964 <patilrohit19649@gmail.com>
2025-02-19 23:37:44 +0530

    added array table for each number

---
+++ array1dPrac_ch10/arrEachNumTable.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "\nMultiplication Tables:\n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Table for " << arr[i] << ":\n";
+        for (int j = 1; j <= 10; j++)
+        {
+            cout << arr[i] << " x " << j << " = " << arr[i] * j << endl;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
