2504d07d842e50c55e8ee8ec3d37803d1c4d6437
patilrohit1964 <patilrohit19649@gmail.com>
2025-02-19 23:44:11 +0530

    reverse array number print

---
+++ array1dPrac_ch10/rev_arr.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "Reverse Number of array: ";
+    for (int i = n - 1; i >= 0; i--)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
