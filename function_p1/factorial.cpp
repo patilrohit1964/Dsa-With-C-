ec14ddc761d5863d0616f2cef382a2693a5a21e7
patilrohit1964 <patilrohit19649@gmail.com>
2025-03-01 23:10:37 +0530

    added function

---
+++ function_p1/factorial.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+long long factorial(int num)
+{
+    long long fact = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        fact *= i;
+    }
+    return fact;
+}
+
+int main()
+{
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    cout << "Factorial of " << num << " is: " << factorial(num) << endl;
+    return 0;
+}
